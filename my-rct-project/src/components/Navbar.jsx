f1e0f010f4a865a95e434cfaefaf847479f59a5a
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-05-04 22:04:03 +0530

    adding

--- my-rct-project/src/components/Navbar.jsx
+++ my-rct-project/src/components/Navbar.jsx
@@ -1,3 +1,24 @@
-const Navbar =()=>{
-    return <h1>This is Navabar page</h1>
+const Navbar = () => {
+    return (
+        <div className="navbar-outer-div" style={{border:"2px solid blue", display:"flex",justifyContent:"space-between"}}>
+            <div style={{border:"2px solid blue", display:"flex",justifyContent:"space-around",width:"30%",paddingLeft:"10px"}}>
+                <div>logo here</div>
+                <div>selecttag here</div>
+            </div>
+            <div style={{border:"2px solid blue", display:"flex",justifyContent:"space-evenly",width:"40%"}}>
+                <div style={{border:"2px solid blue", display:"flex",justifyContent:"space-around",width:"20%"}} className="login-signup">
+                    <div>login</div>
+                    <div>signup</div>
+                </div>
+                <div>
+                    offers
+                </div>
+                <div style={{paddingRight:"10px"}}>
+                    cart
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default Navbar
